b7f51e638f914abf4030ac830f505c2cadbf4ec1
Hamza Osama <hamzaosamaa12@gmail.com>
2026-05-03 20:35:48 +0300

    removing unused userprofile model

--- backend/store/admin.py
+++ backend/store/admin.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
-from .models import Category, Product, Order, OrderItem, UserProfile, Cart, CartItem
+from .models import Category, Product, Order, OrderItem, Cart, CartItem
 
 # Register your models here.
 admin.site.register(Category)
 admin.site.register(Product)
 admin.site.register(Order)
 admin.site.register(OrderItem)
-admin.site.register(UserProfile)
 admin.site.register(Cart)
 admin.site.register(CartItem)
--- backend/store/models.py
+++ backend/store/models.py
@@ -20,14 +20,6 @@ class Product(models.Model):
     def __str__(self):
         return f"{self.name}, {self.discription}"
     
-class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    phone = models.CharField(max_length=15, blank=True)
-    address = models.TextField(blank=True)
-
-    def __str__(self):
-        return self.user.username
-    
 class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     created_at = models.DateField(auto_now_add=True)
