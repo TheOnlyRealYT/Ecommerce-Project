199f94e5f1cb11ac8847261cf737a4022b38b9da
Hamza Osama <hamzaosamaa12@gmail.com>
2026-05-08 12:16:58 +0300

    trying to fix static files

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -139,13 +139,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'media/'
+STATIC_URL = '/static/'
 
 CORS_ALLOWED_ORIGINS = [
     os.getenv('FRONTEND_PORT'),
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 REST_FRAMEWORK = {
