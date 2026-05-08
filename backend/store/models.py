6640e26b354ce3dde8ae2aa9a056df0682d5d504
Hamza Osama <hamzaosamaa12@gmail.com>
2026-05-08 15:13:51 +0300

    migration

--- backend/store/models.py
+++ backend/store/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from cloudinary_storage.storage import MediaCloudinaryStorage
 
 # Create your models here.
 class Category(models.Model):
@@ -14,7 +15,7 @@ class Product(models.Model):
     name = models.CharField(max_length=100, unique=True)
     discription  = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.ImageField(upload_to='imgs/products/', blank=True, null=True)
+    image = models.ImageField(upload_to='imgs/products/', storage=MediaCloudinaryStorage(), blank=True, null=True)
     created_at = models.DateField(auto_now_add=True)
 
     def __str__(self):
